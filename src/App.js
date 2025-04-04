1949247f6a142ab35ebcbb04e67957e08ca0d332
Ethan <ej.barr00@gmail.com>
2025-04-04 07:31:53 -0400

    lazy loading, dynamic importing, and code splitting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, lazy, Suspense } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { checkUserSession } from "./store/user/user.action";
+import { Spinner } from "./components/spinner/spinner.component";
 
-import Shop from "../src/routes/shop/shop.component";
-import Authentication from "./routes/authentication/authentication.component";
-import Checkout from "./routes/checkout/checkout.component";
-import Home from "./routes/home/home.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Payment from "./routes/payment/payment.component";
+// React will not render until it actually fetches this (going to this page) --> reduces the bundle.js size due to code splitting and dynamic imports
+// Went from 854 kB --> 302 B
+const Home = lazy(() => import("./routes/home/home.component"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
+const Shop = lazy(() => import("../src/routes/shop/shop.component"));
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
+const Navigation = lazy(() =>
+  import("./routes/navigation/navigation.component")
+);
+const Payment = lazy(() => import("./routes/payment/payment.component"));
 
 function App() {
   const dispatch = useDispatch();
@@ -18,15 +25,17 @@ function App() {
   });
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path="shop/*" element={<Shop />} />
-        <Route path="auth" element={<Authentication />} />
-        <Route path="checkout" element={<Checkout />} />
-        <Route path="payment" element={<Payment />} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="payment" element={<Payment />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 }
 
